bc0e930a6f6cf0340c171a52c02baa5d3501bd0a
Pranali Patil <patilpranali453@gmail.com>
2026-01-21 12:07:48 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000",
+           "https://pcosmart.vercel.app",
+            "https://pcosmart-mlservices.onrender.com"
+  ],
+
   credentials: true,
 }));
 
